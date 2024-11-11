14a7e2bca3543384e74dc8b5a65dd7bc80bec29a
GailDev10 <GailDev10@gmail.com>
2024-11-11 14:29:23 -0500

    centered videos

--- videos/views.py
+++ videos/views.py
@@ -4,8 +4,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Video
 
-# def index(request):
-# 	return render(request, 'videos/index.html')
+def index(request):
+	return render(request, 'videos/index.html')
 
 class Index(ListView):
 	model = Video
@@ -17,8 +17,8 @@ class CreateVideo(CreateView):
 	fields = ['title', 'description', 'video_file', 'thumbnail']
 	template_name = 'videos/create_video.html'
 
-	# def get_success_url(self):
-	# 	return reverse('video-detail', kwargs={'pk': self.object.pk})
+	def get_success_url(self):
+		return reverse('video-detail', kwargs={'pk': self.object.pk})
 
 class DetailVideo(DetailView):
 	model = Video
